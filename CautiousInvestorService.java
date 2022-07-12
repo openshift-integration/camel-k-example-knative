36720edf4d467b498ace8149d23bbc949f78bc2e
Christoph Deppisch <cdeppisch@redhat.com>
2022-07-12 16:20:56 +0200

    chore: Use the latest Camel K version

--- CautiousInvestorService.java
+++ CautiousInvestorService.java
@@ -3,18 +3,19 @@
 import org.apache.camel.builder.RouteBuilder;
 
 public class CautiousInvestorService extends RouteBuilder {
-  @Override
-  public void configure() throws Exception {
+    @Override
+    public void configure() throws Exception {
 
-      rest().post("/{currency}")
-        .route()
-        .log("Asking the internal department to BUY ${header.currency}...")
-        .setBody(constant(""));
+        rest()
+            .post("/{currency}").to("direct:buy")
+            .delete("/{currency}").to("direct:sell");
 
-      rest().delete("/{currency}")
-        .route()
-        .log("Asking the internal department to SELL ${header.currency}...")
-        .setBody(constant(""));
+        from("direct:buy")
+            .log("Asking the internal department to BUY ${header.currency}...")
+            .setBody(constant(""));
 
-  }
+        from("direct:sell")
+            .log("Asking the internal department to SELL ${header.currency}...")
+            .setBody(constant(""));
+    }
 }
